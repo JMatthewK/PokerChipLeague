a159a1a7ef5e5e10144528aea930d7c553bbe1c9
JMatthewK <matthewkassapian@gmail.com>
2026-09-17 10:45:36 -0400

    Implemented SQLalchemy to create database sessions from our Supabase database, will now work on populating the supabase database when accounts are created or logged into

---
+++ backend/app/database.py
@@ -0,0 +1,34 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+from sqlalchemy.orm import DeclarativeBase
+
+load_dotenv()
+
+# Retrieve the dtaabase URL from the environment variable
+database_url = os.getenv("DATABASE_URL")
+
+if not database_url:
+    raise RuntimeError("DATABASE_URL is not configured")
+
+# Create an asynchronous engine and sessionmaker for the database
+engine = create_async_engine(
+    database_url,
+    echo=False,
+    pool_pre_ping=True,
+)
+
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+
+class Base(DeclarativeBase):
+    pass
